bbc5e14ec2228336279be192e825fc3e222d39e5
Michael Andre Souza da Costa <89527883+MichaelECOMP@users.noreply.github.com>
2024-03-11 23:58:09 -0300

    Initial commit

---
+++ components/Main.jsx
@@ -0,0 +1,23 @@
+const Main = () => {
+    return (
+      <div className="Main">
+        <header>
+            <div className="container-img">
+              <img src='../imagem/DGRoll.png' alt="icone de Logo" />
+            </div>
+        </header>
+        <main className="main-content">
+            <h1>Bem-vindo ao DGRoll!</h1>
+            <p>Explore mundos mágicos, lute contra criaturas lendárias, e torne-se o herói de sua própria jornada épica!</p>
+            <p>DGRoll é um gerenciador de fichas!</p>
+            <p>Você mestre terá em um só lugar as fichas de seus jogadores e NPCs disponíveis a qualquer momento.</p>
+            <p>E vocês jogadores não precisarão se preocupar em guardá-las em seus computadores!</p>
+        </main>
+        <footer>
+            {/* Conteúdo do rodapé */}
+        </footer>
+      </div>
+    );
+  };
+  
+  export default Main;
---
+++ components/TempleteExpressions.jsx
@@ -0,0 +1,11 @@
+const TempleteExpressions = () => {
+    const name = "DGRoll"
+
+    return(
+        <div>
+            <h1>Olá {name}, Tudo bem?</h1>
+        </div>
+    )
+}
+
+export default TempleteExpressions;
